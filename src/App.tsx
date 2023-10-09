bda434355e487c0b27c685f15f26a6c6c26479a6
Hamid Reza Khavarie <hr.khavarie@gmail.com>
2023-10-09 07:59:54 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+    <div className="text-danger">fdsfdfdsf</div>
+      
+    </>
+  )
+}
+
+export default App
